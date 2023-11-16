1c96dd31dcfdfafff4448ce04e92e09167cf10d8
jk15162428 <qweasdzxc501@163.com>
2023-11-16 14:54:23 -0800

    Merge light and player click ability

--- Assets/Scripts/Player/LightAbilityComp.cs
+++ Assets/Scripts/Player/LightAbilityComp.cs
@@ -8,6 +8,8 @@ public class LightAbilityComp : MonoBehaviour
 {
     //This is for debugging feel free to remove this SF 
     [SerializeField] private FOVMeshComp _fovMeshComp;
+    [SerializeField] private float decreaseAngle = 5.0f;
+    [SerializeField] private float shrinkTime = 2.0f;
     private Light2D _lightComp;
     private float _outerAngle;
     
@@ -20,8 +22,19 @@ public class LightAbilityComp : MonoBehaviour
     public void Clap()
     {
         Debug.Log("Light Clapped");
-        _outerAngle -= 5.0f;
-        _lightComp.pointLightOuterAngle = _outerAngle;
-        _fovMeshComp.SetFOV(_outerAngle);
+        _outerAngle -= decreaseAngle;
+        //_lightComp.pointLightOuterAngle = Mathf.SmoothStep(_lightComp.pointLightOuterAngle, _outerAngle, 1f);
+        StartCoroutine(ShrinkLight(shrinkTime));
+        if (_fovMeshComp != null)
+            _fovMeshComp.SetFOV(_outerAngle);
+    }
+
+    IEnumerator ShrinkLight(float shrinkTime) {
+        float elapsedTime = 0.0f;
+        while (elapsedTime < shrinkTime) {
+            _lightComp.pointLightOuterAngle = Mathf.SmoothStep(_lightComp.pointLightOuterAngle, _outerAngle, elapsedTime / shrinkTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
     }
 }
