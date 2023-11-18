96b29ae9b01c953b771cad96f8354aa1dc35b276
huidanz18 <robinzheng1018@gmail.com>
2023-11-17 18:47:41 -0800

    Terrain prefab

--- Assets/Scripts/Shader/TreeShaderPivot.cs
+++ Assets/Scripts/Shader/TreeShaderPivot.cs
@@ -8,6 +8,7 @@ public class TreeShaderPivot : MonoBehaviour
     // Start is called before the first frame update
     public GameObject player;
     public float xScale, yScale;
+    //public float maxDis;
     void Start()
     {
         
@@ -20,7 +21,8 @@ public class TreeShaderPivot : MonoBehaviour
         transform.up = toPlayer;
 
         float dis = toPlayer.magnitude;
-        Vector3 scale = new Vector3(1/dis * xScale, dis * yScale, 1);
+        dis = Mathf.Max(0.8f,Mathf.Min(dis, 2));
+        Vector3 scale = new Vector3(dis * xScale, 1/dis * yScale, 1);
         transform.localScale = scale;
     }
 }
